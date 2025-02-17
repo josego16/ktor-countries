8dd1c800ff06db6bcc85bd0d8c674666cfab957a
josego <josemagomez53@gmail.com>
2025-02-17 12:34:10 +0100

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+import io.ktor.server.application.*
+import io.ktor.server.netty.EngineMain
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureHTTP()
+    configureRouting()
+}
