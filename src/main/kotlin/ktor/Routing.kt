3d587e2e59ee53ebb2deb25e2bb2ffa5425f4301
josego16 <josemagomez53@gmail.com>
2025-02-26 11:19:55 +0100

    reorganizadas las rutas

--- src/main/kotlin/ktor/Routing.kt
+++ src/main/kotlin/ktor/Routing.kt
@@ -13,6 +13,7 @@ import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import ktor.routing.countryRouting
 
 fun Application.configureRouting() {
     install(StatusPages) {
@@ -21,97 +22,7 @@ fun Application.configureRouting() {
         }
     }
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-
-        // Petición GET para obtener países
-        get("/country") {
-            val countryPid = call.request.queryParameters["pid"]
-            logger.warn("El pid tiene valor de $countryPid")
-            if (countryPid != null) {
-                val country = ProviderUseCase.countryById(countryPid)
-                if (country == null) {
-                    call.respond(HttpStatusCode.NotFound, "País no encontrado")
-                } else {
-                    call.respond(country)
-                }
-                return@get
-            }
-
-            val language = call.request.queryParameters["language"]
-            logger.warn("Language tiene valor de $language")
-            if (language != null) {
-                try {
-                    val lang = language.uppercase()
-                    logger.warn("Lenguaje seleccionado: ${Language.valueOf(lang)}")
-                    val countries = ProviderUseCase.countryByLanguage(Language.valueOf(lang.uppercase()))
-                    call.respond(countries)
-                } catch (e: IllegalArgumentException) {
-                    call.respond(HttpStatusCode.BadRequest, "Lenguaje no válido")
-                }
-            } else {
-                val allCountries = ProviderUseCase.allCountries()
-                call.respond(allCountries)
-            }
-        }
-
-        // Petición POST para agregar un nuevo país
-        post("/country") {
-            try {
-                val country = call.receive<Country>()
-                val newCountry = ProviderUseCase.addCountry(country)
-                if (!newCountry) {
-                    call.respond(HttpStatusCode.Conflict, "El pais no ha podido insertarse, puede que ya exista")
-                    return@post
-                }
-                call.respond(HttpStatusCode.Created, "Se ha insertado correctamente con pid = ${country.pid}")
-            } catch (error: IllegalArgumentException) {
-                call.respond(HttpStatusCode.BadRequest, error.localizedMessage)
-            } catch (error: JsonConvertException) {
-                call.respond(HttpStatusCode.BadRequest, error.localizedMessage)
-            }
-        }
-
-        // Petición PATCH para editar un país
-        patch("/country/{pid}") {
-            try {
-                val countryPid = call.parameters["pid"]
-                countryPid?.let {
-                    val updatedData = call.receive<UpdateCountry>()
-                    val updatedCountry = ProviderUseCase.updateCountry(updatedData, countryPid)
-                    if (!updatedCountry) {
-                        call.respond(HttpStatusCode.Conflict, "El pais no puede modificarse, puede que ya exista")
-                        return@patch
-                    }
-                    call.respond(HttpStatusCode.Created, "Se ha modificado correctamente con pid = $countryPid")
-                } ?: run {
-                    call.respond(HttpStatusCode.BadRequest, "Debes identificar el pais")
-                    return@patch
-                }
-            } catch (error: IllegalArgumentException) {
-                call.respond(HttpStatusCode.BadRequest, error.localizedMessage)
-            } catch (error: JsonConvertException) {
-                call.respond(HttpStatusCode.BadRequest, error.localizedMessage)
-            }
-        }
-
-        // Petición DELETE para eliminar un país
-        delete("/country/{pid}") {
-            val countryPid = call.parameters["pid"]
-            logger.warn("Queremos borrar el pais con el $countryPid")
-            countryPid?.let {
-                val deleted = ProviderUseCase.deleteCountry(countryPid)
-                if (!deleted) {
-                    call.respond(HttpStatusCode.NotFound, "País no encontrado para borrar")
-                } else {
-                    call.respond(HttpStatusCode.NoContent)
-                }
-            } ?: run {
-                call.respond(HttpStatusCode.BadRequest, "Debes identificar el pais")
-            }
-            return@delete
-        }
+        countryRouting()
         // Static plugin. Try to access `/static/index.html`
         staticResources("/static", "static")
     }
---
+++ src/main/kotlin/ktor/routing/CountryRouting.kt
@@ -0,0 +1,110 @@
+package ktor.routing
+
+import domain.country.models.country.Country
+import domain.country.models.country.UpdateCountry
+import domain.country.models.enums.Language
+import domain.country.usecase.ProviderUseCase
+import domain.country.usecase.ProviderUseCase.logger
+import io.ktor.http.*
+import io.ktor.serialization.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Routing.countryRouting() {
+    route("/country") {
+
+        // Peticion GET para obtener todos los paises
+        get {
+            val country = ProviderUseCase.allCountries()
+            call.respond(country)
+        }
+        // Petición GET para obtener un pais
+        get("/{pid}") {
+            val countryPid = call.parameters["pid"]
+            logger.warn("El pid tiene valor de $countryPid")
+            if (countryPid != null) {
+                val country = ProviderUseCase.countryById(countryPid)
+                if (country == null) {
+                    call.respond(HttpStatusCode.NotFound, "País no encontrado")
+                } else {
+                    call.respond(country)
+                }
+                return@get
+            }
+
+            val language = call.parameters["language"]
+            logger.warn("Language tiene valor de $language")
+            if (language != null) {
+                try {
+                    val lang = language.uppercase()
+                    logger.warn("Lenguaje seleccionado: ${Language.valueOf(lang)}")
+                    val countries = ProviderUseCase.countryByLanguage(Language.valueOf(lang.uppercase()))
+                    call.respond(countries)
+                } catch (e: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest, "Lenguaje no válido")
+                }
+            } else {
+                val allCountries = ProviderUseCase.allCountries()
+                call.respond(allCountries)
+            }
+        }
+
+        // Petición POST para agregar un nuevo país
+        post {
+            try {
+                val country = call.receive<Country>()
+                val newCountry = ProviderUseCase.addCountry(country)
+                if (!newCountry) {
+                    call.respond(HttpStatusCode.Conflict, "El pais no ha podido insertarse, puede que ya exista")
+                    return@post
+                }
+                call.respond(HttpStatusCode.Created, "Se ha insertado correctamente con pid = ${country.pid}")
+            } catch (error: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest, error.localizedMessage)
+            } catch (error: JsonConvertException) {
+                call.respond(HttpStatusCode.BadRequest, error.localizedMessage)
+            }
+        }
+
+        // Petición PATCH para editar un país
+        patch("/{pid}") {
+            try {
+                val countryPid = call.parameters["pid"]
+                countryPid?.let {
+                    val updatedData = call.receive<UpdateCountry>()
+                    val updatedCountry = ProviderUseCase.updateCountry(updatedData, countryPid)
+                    if (!updatedCountry) {
+                        call.respond(HttpStatusCode.Conflict, "El pais no puede modificarse, puede que ya exista")
+                        return@patch
+                    }
+                    call.respond(HttpStatusCode.Created, "Se ha modificado correctamente con pid = $countryPid")
+                } ?: run {
+                    call.respond(HttpStatusCode.BadRequest, "Debes identificar el pais")
+                    return@patch
+                }
+            } catch (error: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest, error.localizedMessage)
+            } catch (error: JsonConvertException) {
+                call.respond(HttpStatusCode.BadRequest, error.localizedMessage)
+            }
+        }
+
+        // Petición DELETE para eliminar un país
+        delete("/{pid}") {
+            val countryPid = call.parameters["pid"]
+            logger.warn("Queremos borrar el pais con el $countryPid")
+            countryPid?.let {
+                val deleted = ProviderUseCase.deleteCountry(countryPid)
+                if (!deleted) {
+                    call.respond(HttpStatusCode.NotFound, "País no encontrado para borrar")
+                } else {
+                    call.respond(HttpStatusCode.NoContent)
+                }
+            } ?: run {
+                call.respond(HttpStatusCode.BadRequest, "Debes identificar el pais")
+            }
+            return@delete
+        }
+    }
+}
