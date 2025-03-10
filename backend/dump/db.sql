8c883f51d456b09cab1f6cf6bcdff2315bf750bd
josego16 <josemagomez53@gmail.com>
2025-03-10 10:29:18 +0100

    seguimos corrigiendo errores

--- backend/dump/db.sql
+++ backend/dump/db.sql
@@ -26,46 +26,29 @@ CREATE TABLE Country
     typicalGastronomy VARCHAR(255)       NOT NULL,
     flag_url          VARCHAR(255)       NULL
 );
-
--- Agregar la columna owner_id permitiendo valores NULL temporalmente
-ALTER TABLE Country
-    ADD COLUMN owner_id INT NULL;
-
--- Asignar un owner_id a los países existentes (se asigna al primer usuario disponible)
-UPDATE Country
-SET owner_id = (SELECT id FROM User ORDER BY id LIMIT 1);
-
--- Modificar la columna owner_id para que sea NOT NULL
-ALTER TABLE Country
-    MODIFY COLUMN owner_id INT NOT NULL;
-
--- Agregar la clave foránea con restricción ON DELETE CASCADE
-ALTER TABLE Country
-    ADD CONSTRAINT fk_country_user FOREIGN KEY (owner_id) REFERENCES User (id) ON DELETE CASCADE;
-
 -- Insertar datos en la tabla Country con la nueva estructura
-INSERT INTO Country (pid, name, capital, language, famousEvent, typicalGastronomy, flag_url, owner_id)
+INSERT INTO Country (pid, name, capital, language, famousEvent, typicalGastronomy, flag_url)
 VALUES ('FR01', 'France', 'Paris', 'French', 'Tour de France', 'Baguette, Ratatouille, Croissant',
-        'https://flagcdn.com/w320/fr.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/fr.png'),
        ('US02', 'United States', 'Washington D.C.', 'English', 'Super Bowl', 'Hamburger, Hot Dog, BBQ Ribs',
-        'https://flagcdn.com/w320/us.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/us.png'),
        ('JP03', 'Japan', 'Tokyo', 'Japanese', 'Cherry Blossom Festival', 'Sushi, Ramen, Tempura',
-        'https://flagcdn.com/w320/jp.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/jp.png'),
        ('DE04', 'Germany', 'Berlin', 'German', 'Oktoberfest', 'Bratwurst, Pretzel, Schnitzel',
-        'https://flagcdn.com/w320/de.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/de.png'),
        ('BR05', 'Brazil', 'Brasilia', 'Portuguese', 'Carnival of Rio', 'Feijoada, Pão de Queijo, Açaí',
-        'https://flagcdn.com/w320/br.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/br.png'),
        ('KR06', 'South Korea', 'Seoul', 'Korean', 'Boryeong Mud Festival', 'Kimchi, Bibimbap, Bulgogi',
-        'https://flagcdn.com/w320/kr.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/kr.png'),
        ('CN07', 'China', 'Beijing', 'Chinese', 'Chinese New Year', 'Peking Duck, Dim Sum, Hot Pot',
-        'https://flagcdn.com/w320/cn.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/cn.png'),
        ('ES08', 'Spain', 'Madrid', 'Spanish', 'Running of the Bulls', 'Paella, Tapas, Jamón Ibérico',
-        'https://flagcdn.com/w320/es.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/es.png'),
        ('RU09', 'Russia', 'Moscow', 'Russian', 'Victory Day Parade', 'Borscht, Blini, Pelmeni',
-        'https://flagcdn.com/w320/ru.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/ru.png'),
        ('IT10', 'Italy', 'Rome', 'Italian', 'Venice Carnival', 'Pizza, Pasta, Gelato',
-        'https://flagcdn.com/w320/it.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/it.png'),
        ('UK11', 'United Kingdom', 'London', 'English', 'Wimbledon', 'Fish and Chips, Full English Breakfast',
-        'https://flagcdn.com/w320/gb.png', (SELECT id FROM User ORDER BY id LIMIT 1)),
+        'https://flagcdn.com/w320/gb.png'),
        ('AU12', 'Australia', 'Canberra', 'English', 'Melbourne Cup', 'Vegemite, Meat Pie, Pavlova',
-        'https://flagcdn.com/w320/au.png', (SELECT id FROM User ORDER BY id LIMIT 1));
+        'https://flagcdn.com/w320/au.png');
